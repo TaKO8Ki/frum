8c9c5229ea0392afe4d53bb1c1d18c0cf7dcb5a6
Takayuki <takoyaki0316@gmail.com>
2021-04-02 16:54:12 +0900

    add windows infer

--- src/shell/mod.rs
+++ src/shell/mod.rs
@@ -15,6 +15,12 @@ pub trait Shell: Debug {
     fn use_on_cd(&self, config: &crate::config::FarmConfig) -> String;
 }
 
+#[cfg(windows)]
+pub fn infer_shell() -> Option<Box<dyn Shell>> {
+    self::infer::windows::infer_shell()
+}
+
+#[cfg(unix)]
 pub fn infer_shell() -> Option<Box<dyn Shell>> {
     infer::unix::infer_shell()
 }
