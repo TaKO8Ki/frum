d17b452080f712034863786273bcd23c5886820b
Takayuki <takoyaki0316@gmail.com>
2021-04-19 15:07:55 +0900

    add e2e tests for uninstall command

--- tests/command_tests/mod.rs
+++ tests/command_tests/mod.rs
@@ -1,10 +1,7 @@
 use crate::{e2e_test, eq, eq_re};
 
 e2e_test!(use_installed_version, |dir| {
-    eq!(
-        "==> Installing Ruby 2.7.1\n",
-        dir.command().arg("install").arg("2.7.1").stdout()
-    );
+    dir.command().arg("install").arg("2.7.1").output();
     eq!("", dir.command().arg("local").arg("2.7.1").stdout());
     eq_re!("^ruby 2.7.1", dir.ruby_version());
     eq!("", dir.command().arg("global").arg("2.7.1").stdout());
@@ -13,4 +10,14 @@ e2e_test!(use_installed_version, |dir| {
 
 e2e_test!(use_not_installed_version, |dir| {
     dir.command().arg("local").arg("2.0.0").assert_err();
+});
+
+e2e_test!(uninstall_installed_version, |dir| {
+    dir.command().arg("install").arg("2.7.1").output();
+    dir.command().arg("uninstall").arg("2.7.1").output();
+});
+
+e2e_test!(uninstall_not_installed_version, |dir| {
+    dir.command().arg("install").arg("2.7.1").output();
+    dir.command().arg("uninstall").arg("2.6.5").assert_err();
 });
